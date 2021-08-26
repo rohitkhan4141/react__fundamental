d1e8460a253a6ffce65653742b026d983578f1e7
rohitkhan4141 <rohitkhan4141@gmail.com>
2021-08-26 07:54:22 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import Hello from "./components/Hello";
+
 function App() {
   return <div className="App">hello1</div>;
+  // eslint-disable-next-line no-unreachable
+  <Hello />;
 }
 
 export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <div>i am a hello function</div>;
+};
+
+export default Hello;
